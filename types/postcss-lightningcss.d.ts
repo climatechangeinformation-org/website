3299c19baaf804ae6f0b69b96b2c9f2e1142cc39
Daniel Miao <danielmiao2019@icloud.com>
2024-01-06 17:42:05 -0800

    minify distribution CSS

---
+++ types/postcss-lightningcss.d.ts
@@ -0,0 +1,5 @@
+declare module 'postcss-lightningcss' {
+	import { Plugin } from "postcss";
+
+	export default function lightningCss(partialOptions: {[key: string]: any}): Plugin
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+import { browserslistToTargets } from 'lightningcss';
+import lightningCss from "postcss-lightningcss";
 import solidPlugin from "vite-plugin-solid";
 import legacy from "@vitejs/plugin-legacy";
+import browserslist from 'browserslist';
 import { defineConfig } from "vite";
-import { resolve } from "path";
 
 export default defineConfig({
 	plugins: [
@@ -14,5 +16,16 @@ export default defineConfig({
 	],
 	server: {
 		port: 3000,
+	},
+	css: {
+		postcss: {
+			plugins: [
+				lightningCss({
+					lightningcssOptions: {
+						targets: browserslistToTargets(browserslist("last 30 versions or > 0.01% or not dead or last 3 IE versions"))
+					}
+				})
+			]
+		}
 	}
 });
