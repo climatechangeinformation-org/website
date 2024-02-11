a66519b1683f42c245b49392d7ca80bad352c0ea
Daniel Miao <danielmiao2019@icloud.com>
2024-02-10 20:44:52 -0800

    implement historical data chart

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,33 @@
 import type { Component } from "solid-js";
+import { onMount } from "solid-js";
 
 import "./Home.scss"
 import Navigation from "../../components/NavBar";
 
 import globe from "../../assets/globe.webp";
+import co2_historical from "../../assets/co2_historical.json?url";
+
+import {
+	Chart,
+	Tooltip,
+	LinearScale,
+	LineElement,
+	PointElement,
+	CategoryScale,
+	LineController,
+} from "chart.js";
+
+Chart.register(
+	Tooltip,
+	LinearScale,
+	LineElement,
+	PointElement,
+	CategoryScale,
+	LineController
+);
+
+Chart.defaults.color = "#fff";
+Chart.defaults.borderColor = "#fff8";
 
 // import satellite_map from "../../assets/land_shallow_topo_15360.webp"; // NASA Goddard Space Flight Center; Reto Stöckli; Robert Simmon.
 
@@ -13,12 +37,57 @@ import globe from "../../assets/globe.webp";
 
 // import cloud_map from "../../assets/cloud_combined_8192.webp" // NASA Goddard Space Flight Center; Reto Stöckli; Robert Simmon.
 
+interface ICO2Historical {
+	year: string,
+	co2: number,
+	map: typeof Array.prototype.map
+}
+
 const Home: Component = () => {
-	document.getElementsByTagName("main")[0]!.style.backgroundImage = `url(${globe})`
-	
+	document.getElementsByTagName("main")[0]!.style.backgroundImage = `url(${globe})`;
+
+	let co2_chart: HTMLCanvasElement | undefined;
+
+	onMount(() => fetch(co2_historical).then(async response => {
+		const data: ICO2Historical = (await response.json()).reverse();
+		new Chart(co2_chart, {
+			type: "line",
+			data: {
+				labels: data.map(row => row.year),
+				datasets: [{
+					data: data.map(row => row.co2),
+					borderColor: "#fff"
+				}]
+			},
+			options: {
+				animation: false,
+				elements: {
+					point: {
+						pointStyle: false
+					}
+				},
+				maintainAspectRatio: false,
+				plugins: {
+					tooltip: {
+						mode: "index",
+						intersect: false
+					}
+				},
+				scales: {
+					y: {
+						beginAtZero: true
+					}
+				}
+			}
+		})
+	}));
+
 	return (
 		<>
 			<Navigation/>
+			<div class="chart-container">
+				<canvas ref={co2_chart}></canvas>
+			</div>
 		</>
 	);
 };
