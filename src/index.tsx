db598c9a6d2a1c0111d74c90eafd3a8cf3f9efda
Daniel Miao <danielmiao2019@icloud.com>
2024-02-25 09:48:31 -0800

    load sentry asynchronously

--- src/index.tsx
+++ src/index.tsx
@@ -2,24 +2,26 @@ import { render } from "solid-js/web";
 import { Router, Route } from "@solidjs/router";
 import { lazy } from "solid-js";
 
-import * as Sentry from "@sentry/browser";
-
 import "./index.scss";
 
 import Home from "./pages/Home/Home";
 
+import("@sentry/browser").then(Sentry => {
+	const SENTRY_SUBDOMAIN = "88a310cd3fb4f41264c4770d72d65ef3@o4505149986242560";
+
+	Sentry.init({
+		dsn: `https://${SENTRY_SUBDOMAIN}.ingest.sentry.io/4506748840771584`,
+		integrations: [
+			Sentry.browserTracingIntegration()
+		],
+		tracesSampleRate: 1.0
+	});
+}).catch(error => {
+	console.log("Could not import sentry:", error);
+});
+
 const Impacts = lazy(() => import("./pages/Impacts/Impacts"));
 const Causes = lazy(() => import("./pages/Causes/Causes"));
-
-const SENTRY_SUBDOMAIN = "88a310cd3fb4f41264c4770d72d65ef3@o4505149986242560";
-
-Sentry.init({
-	dsn: `https://${SENTRY_SUBDOMAIN}.ingest.sentry.io/4506748840771584`,
-	integrations: [
-		Sentry.browserTracingIntegration()
-	],
-	tracesSampleRate: 1.0
-});
 
 render(
 	() => <Router>
